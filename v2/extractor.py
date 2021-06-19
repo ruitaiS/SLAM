68924d4243b3a6dffd0e7863359fd8e82e4ea114
ruitaiS <shaoruitai@gmail.com>
2021-06-19 18:20:34 -0400

    cleaned up feature matching control flow

--- v2/extractor.py
+++ v2/extractor.py
@@ -15,27 +15,24 @@ class FeatureExtractor(object):
         #Feature Extraction
         feats = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance = 3)
 
-        #Compute Keypoints
-        if feats is not None:
-            kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in feats]
-        else:
-            kps = []
-
-        #Compute Keypoint, Descriptor pairs from Keypoints
-        kps, des = self.orb.compute(img, kps)
-
-        #Matching
-        matches = None
-        if self.last is None:
-            self.last = {'kps': kps, 'des': des}
+        if feats is None:
             return None
+        else:
+            #Compute Keypoint, Descriptor Pairs
+            kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in feats]
+            kps, des = self.orb.compute(img, kps)
 
-        #Generate match pairs between keypoints on current and previous frames
-        matches = self.bf.match(des, self.last['des'])
-        res = zip([kps[m.queryIdx] for m in matches], [self.last["kps"][m.trainIdx] for m in matches])
+            if self.last is None:
+                #If no last, then update w/ current and return w/o matching
+                self.last = {'kps': kps, 'des': des}
+                return None
+            else:
+                #Generate match pairs between keypoints on current and previous frames
+                matches = self.bf.match(des, self.last['des'])
+                res = zip([kps[m.queryIdx] for m in matches], [self.last["kps"][m.trainIdx] for m in matches])
 
-        #Update last
-        self.last = {'kps': kps, 'des': des}
+                #Update last
+                self.last = {'kps': kps, 'des': des}
 
-        #Return matched points
-        return res
+                #Return matched points
+                return res
